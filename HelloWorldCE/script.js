3123d7b1039ac3f9f2e04070bbc746c0ca20e976
xyKausality <MichaelU0405@gmail.com>
2024-10-22 17:46:05 -0400

    Removed multiple files to create a final version of the user-input popup.

---
+++ HelloWorldCE/script.js
@@ -0,0 +1,4 @@
+document.getElementById('displayButton').addEventListener('click', function() {
+    const inputText = document.getElementById('inputField').value;
+    document.getElementById('outputField').value = inputText;
+});
