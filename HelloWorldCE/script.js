51c588f474fb2f6a2cafc8663b2cd3ec3303dd59
Michael U. <MichaelU0405@gmail.com>
2024-10-29 18:08:49 -0400

    Removed files as I am fixing the git structure.

--- HelloWorldCE/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('displayButton').addEventListener('click', function() {
-    const inputText = document.getElementById('inputField').value;
-    document.getElementById('outputField').value = inputText;
-});
