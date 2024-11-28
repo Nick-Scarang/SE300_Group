98a842c97e9a6ccd936f826961beaa0473c19882
Nick-Scarang <scarangn@my.erau.edu>
2024-11-28 15:52:34 -0500

    Changed Master

--- js/Master.js
+++ js/Master.js
@@ -9,6 +9,7 @@ class Master {
     initialize() {
         console.log("Master class initialized");
         this.UI = new UserInterface();
+        this.CI = new CanvasInterface(this);
         this.CI.fetchCourses().then(() => {
             console.log('Courses fetched:', this.CI.courses);
             this.UI.showMainMenu();
